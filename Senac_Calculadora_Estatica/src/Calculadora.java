ddb09d1baf966b2c8f5ea260e042905ca7bac949
WagnerSousaLima <wagner.sousalima@gmail.com>
2022-05-13 03:02:57 -0300

    Projeto Java de soma simples usando static

---
+++ Senac_Calculadora_Estatica/src/Calculadora.java
@@ -0,0 +1,8 @@
+
+public class Calculadora {
+	public static int somar(int x, int y) {
+		int resultado = x +y ;
+		return resultado;
+	}
+
+}
---
+++ Senac_Calculadora_Estatica/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int res = Calculadora.somar(50, 25);
+		System.out.println(res);
+
+	
+
+}
+}
